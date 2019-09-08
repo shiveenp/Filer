738abe501d7f7edb3daeb5d63e5a51b71741180a
shiveenpandita <shiveenpandita@gmail.com>
2019-09-08 20:36:00 +1000

    add sync mode and refactor existing upload and download modes to be composable

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use std::io::{Read, Write};
 use std::{env, fs};
 use walkdir::{DirEntry, WalkDir};
 
+enum SyncServices {
+    S3
+}
+
 enum SyncType {
     Upload,
     Download,
@@ -15,7 +19,7 @@ enum SyncType {
 fn main() {
     let bucket_name = "brows3r-test";
     let region = "ap-southeast-2";
-    let test_directory = "/Users/Shavz/rust-watcher-test/";
+    let test_directory = "/Users/shiveenp/dev/rust-file-watcher-test/";
     let delete_flag = true;
     let aws_region: Region = region.parse().unwrap();
     let credentials: Credentials = Credentials::new(None, None, None, None);
@@ -58,14 +62,26 @@ fn run_upload(test_directory: &str, delete_flag: bool, test_bucket: &Bucket) {
                     let mut data_file = data_file_result.unwrap();
                     let mut data_buffer = Vec::new();
                     let upload_file_path = entry.path().file_name().unwrap().to_str().unwrap();
-                    let (data, check_if_files_exists) = test_bucket.get_object(upload_file_path).unwrap();
-                    if check_if_files_exists != 200 {
+                    println!("checking if {} exists", upload_file_path);
+                    let file_does_not_exist = test_bucket.get_object(upload_file_path).is_err();
+                    if file_does_not_exist {
+                        println!("file not found, uploading {}", upload_file_path);
                         data_file.read_to_end(&mut data_buffer);
-                        test_bucket.put_object(
-                            upload_file_path,
-                            data_buffer.as_ref(),
-                            "text/plain",
-                        );
+                        if (upload_file_path.eq("Brows3r.mov")) {
+                            println!("uploading with new mime type");
+                            let (result, code) = test_bucket.put_object(
+                                upload_file_path,
+                                data_buffer.as_ref(),
+                                "video/quicktime",
+                            ).unwrap();
+                            println!("upload code is {}", code);
+                        } else {
+                            test_bucket.put_object(
+                                upload_file_path,
+                                data_buffer.as_ref(),
+                                "text/plain",
+                            );
+                        }
                         // delete file when done if the delete flag is set
                         if delete_flag {
                             fs::remove_file(entry.path());
@@ -90,10 +106,15 @@ fn run_download(test_directory: &str, test_bucket: &Bucket) {
     for (list, code) in s3_files_list {
         assert_eq!(200, code);
         for content in &list.contents {
+            println!("checking for downloads");
+            println!("checking if {} exists", content.key);
             if !current_dir_files_list.contains(&content.key) {
+                println!("downloading");
                 let (data, code) = test_bucket.get_object(content.key.as_ref()).unwrap();
+                println!("code was {}", code);
                 let mut buffer = File::create(test_directory.to_string() + content.key.as_ref()).unwrap();
-                buffer.write(data.as_ref());
+                let file_write_result = buffer.write(data.as_ref());
+                println!("file write result is {}", file_write_result.is_ok())
             }
         }
     }
