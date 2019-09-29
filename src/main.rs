d5f0a1fcce548b73135b942d5ce5a8a7fd435ce5
shavz <shiveen.pandita@growsuper.com>
2019-09-29 17:58:25 +1000

    Add initial cli options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate human_panic;
+
+use clap::App;
 use s3::bucket::Bucket;
 use s3::credentials::Credentials;
 use s3::region::Region;
@@ -7,91 +13,99 @@ use std::{env, fs, thread, time};
 use walkdir::{DirEntry, WalkDir};
 
 enum SyncServices {
-    S3
+    S3,
 }
 
-enum SyncType {
-    Upload,
-    Download,
-    Sync,
-}
+static ALLOWED_SYNC_TYPES: Vec<&str> = vec!["upload", "download", "replicate"];
 
 fn main() {
-    let bucket_name = "brows3r-test";
+    setup_panic!();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let bucket_name = matches.value_of("bucket").unwrap_or("");
+    let region = matches.value_of("awsregion").unwrap_or("");
+    let sync_type = matches.value_of("").unwrap_or("");
+
     let region = "ap-southeast-2";
-    let test_directory = "/Users/shiveenp/dev/rust-file-watcher-test/";
     let delete_flag = true;
     let aws_region: Region = region.parse().unwrap();
     let credentials: Credentials = Credentials::new(None, None, None, None);
     let test_bucket: Bucket = Bucket::new(bucket_name, aws_region, credentials).unwrap();
-    let mode = SyncType::Sync;
+    let mode = SyncType::Replicate;
     let source = "directory";
     run_s3_sync(test_directory, delete_flag, &test_bucket, mode, source)
 }
 
-fn run_s3_sync(test_directory: &str, delete_flag: bool, test_bucket: &Bucket, mode: SyncType, source: &str) {
+fn validate_user_input(sync_type: &str) {
+    if !ALLOWED_SYNC_TYPES.contains(&sync_type) {
+        panic!("Unable to recognise given synctype, only following values are accepted: upload, download, replicate")
+    }
+}
+
+fn run_s3_sync(
+    test_directory: &str,
+    delete_flag: bool,
+    test_bucket: &Bucket,
+    mode: &str,
+    source: &str,
+) {
     let sleep_time = time::Duration::from_secs(1000);
     loop {
-        match mode {
-            SyncType::Upload => {
-                run_upload(test_directory, delete_flag, &test_bucket)
-            }
+        match mode.to_lowercase() {
+            &"upload" => run_upload(test_directory, delete_flag, &test_bucket),
 
-            SyncType::Download => {
-                run_download(test_directory, test_bucket)
-            }
+            &"download" => run_download(test_directory, test_bucket),
 
-            SyncType::Sync => {
-                run_sync(test_directory, test_bucket, source)
+            &"replicate" => run_sync(test_directory, test_bucket, source),
+            _ => {
+                panic!("Unable to recognise given sync type");
             }
         }
-//        thread::sleep(sleep_time);
+        //        thread::sleep(sleep_time);
     }
 }
 
 fn run_upload(test_directory: &str, delete_flag: bool, test_bucket: &Bucket) {
-    for entry in WalkDir::new(test_directory)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        {
-            let md = entry.metadata().unwrap();
-            if md.is_file() && !entry.file_name().to_str().unwrap().starts_with(".") {
-                let data_file_result = File::open(entry.path());
-                let was_ok = data_file_result.is_ok();
-                if was_ok {
-                    let mut data_file = data_file_result.unwrap();
-                    let mut data_buffer = Vec::new();
-                    let upload_file_path = entry.path().file_name().unwrap().to_str().unwrap();
-                    println!("checking if {} exists", upload_file_path);
-                    let file_does_not_exist = test_bucket.get_object(upload_file_path).is_err();
-                    if file_does_not_exist {
-                        println!("file not found, uploading {}", upload_file_path);
-                        data_file.read_to_end(&mut data_buffer);
-                        test_bucket.put_object(
-                            upload_file_path,
-                            data_buffer.as_ref(),
-                            "text/plain",
-                        );
+    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+        let md = entry.metadata().unwrap();
+        if md.is_file() && !entry.file_name().to_str().unwrap().starts_with(".") {
+            let data_file_result = File::open(entry.path());
+            let was_ok = data_file_result.is_ok();
+            if was_ok {
+                let mut data_file = data_file_result.unwrap();
+                let mut data_buffer = Vec::new();
+                let upload_file_path = entry.path().file_name().unwrap().to_str().unwrap();
+                println!("checking if {} exists", upload_file_path);
+                let file_does_not_exist = test_bucket.get_object(upload_file_path).is_err();
+                if file_does_not_exist {
+                    println!("file not found, uploading {}", upload_file_path);
+                    data_file.read_to_end(&mut data_buffer);
+                    test_bucket.put_object(upload_file_path, data_buffer.as_ref(), "text/plain");
 
-                        // delete file when done if the delete flag is set
-                        if delete_flag {
-                            fs::remove_file(entry.path());
-                        }
+                    // delete file when done if the delete flag is set
+                    if delete_flag {
+                        fs::remove_file(entry.path());
                     }
                 }
             }
         }
+    }
 }
 
 fn run_download(test_directory: &str, test_bucket: &Bucket) {
     // gets the list of files from s3 and scans the dir to see which files aren't present and downloads them
     let mut current_dir_files_list: Vec<String> = Vec::new();
-    for entry in WalkDir::new(test_directory)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        {
-            current_dir_files_list.push(entry.path().file_name().unwrap().to_str().unwrap().to_owned())
-        }
+    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+        current_dir_files_list.push(
+            entry
+                .path()
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_owned(),
+        )
+    }
 
     let s3_files_list = test_bucket.list("", Some("")).unwrap();
     for (list, code) in s3_files_list {
@@ -103,7 +117,8 @@ fn run_download(test_directory: &str, test_bucket: &Bucket) {
                 println!("downloading");
                 let (data, code) = test_bucket.get_object(content.key.as_ref()).unwrap();
                 println!("code was {}", code);
-                let mut buffer = File::create(test_directory.to_string() + content.key.as_ref()).unwrap();
+                let mut buffer =
+                    File::create(test_directory.to_string() + content.key.as_ref()).unwrap();
                 let file_write_result = buffer.write(data.as_ref());
                 println!("file write result is {}", file_write_result.is_ok())
             }
@@ -119,15 +134,20 @@ fn run_sync(test_directory: &str, test_bucket: &Bucket, sync_source: &str) {
 
             // then delete everything that is in s3 but not in the directory
             let mut current_dir_files_list: Vec<String> = Vec::new();
-            for entry in WalkDir::new(test_directory)
-                .into_iter()
-                .filter_map(|e| e.ok())
-                {
-                    let md = entry.metadata().unwrap();
-                    if md.is_file() && !entry.file_name().to_str().unwrap().starts_with(".") {
-                        current_dir_files_list.push(entry.path().file_name().unwrap().to_str().unwrap().to_owned())
-                    }
+            for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+                let md = entry.metadata().unwrap();
+                if md.is_file() && !entry.file_name().to_str().unwrap().starts_with(".") {
+                    current_dir_files_list.push(
+                        entry
+                            .path()
+                            .file_name()
+                            .unwrap()
+                            .to_str()
+                            .unwrap()
+                            .to_owned(),
+                    )
                 }
+            }
 
             let s3_files_list = test_bucket.list("", Some("")).unwrap();
 
@@ -143,15 +163,20 @@ fn run_sync(test_directory: &str, test_bucket: &Bucket, sync_source: &str) {
 
         "s3" => {
             let mut current_dir_files_list: Vec<String> = Vec::new();
-            for entry in WalkDir::new(test_directory)
-                .into_iter()
-                .filter_map(|e| e.ok())
-                {
-                    let md = entry.metadata().unwrap();
-                    if md.is_file() && !entry.file_name().to_str().unwrap().starts_with(".") {
-                        current_dir_files_list.push(entry.path().file_name().unwrap().to_str().unwrap().to_owned())
-                    }
+            for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+                let md = entry.metadata().unwrap();
+                if md.is_file() && !entry.file_name().to_str().unwrap().starts_with(".") {
+                    current_dir_files_list.push(
+                        entry
+                            .path()
+                            .file_name()
+                            .unwrap()
+                            .to_str()
+                            .unwrap()
+                            .to_owned(),
+                    )
                 }
+            }
 
             let s3_files_list = test_bucket.list("", Some("")).unwrap();
 
@@ -160,12 +185,17 @@ fn run_sync(test_directory: &str, test_bucket: &Bucket, sync_source: &str) {
 
             // remove
             for (list, code) in s3_files_list {
-                let file_keys = &list.contents.into_iter().map(|x|
-                    x.key.to_string())
+                let file_keys = &list
+                    .contents
+                    .into_iter()
+                    .map(|x| x.key.to_string())
                     .collect::<Vec<String>>();
                 println!("file keys are {:?}", file_keys);
                 for current_dir_file in &current_dir_files_list {
-                    println!("checking current dir file: {} not inside file list", current_dir_file);
+                    println!(
+                        "checking current dir file: {} not inside file list",
+                        current_dir_file
+                    );
                     if !file_keys.contains(&current_dir_file) {
                         println!("removing file {}", current_dir_file);
                         fs::remove_file(format!("{}/{}", test_directory, current_dir_file));
